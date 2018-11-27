37ed6e5bfb9c3f438471c55273fafb89dd024d93
Shania Bute <sbute02@gmail.com>
2018-11-27 21:22:42 +0000

    shay

--- js/script.js
+++ js/script.js
@@ -4,11 +4,55 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-$("button").click(function(){100
-    $("#userChoice").text($("input").val());
-}); 
-// DOCUMENT READY FUNCTION BELOW
 var userChoice=""
-var computerChoice=""
 var winner=""
-var randomNumber="0"
+var randomNumber="0" 
+var computerChoice= ""
+
+$("button").click(function() {
+    var userChoice = $("#input").val();
+       $("#userChoice").text(userChoice);
+    
+     
+       var computerChoiceName;
+       
+       var computerChoice=Math.floor(Math.random()*3);
+       console.log(computerChoice)
+     if (computerChoice === 0) {
+        $("#text").html ("rock");
+        computerChoiceName="rock"
+    }  
+    
+     if (computerChoice === 1 ) {
+        $("#text").html ("Scissors"); 
+        computerChoiceName="scissors"
+    }  
+
+     if (computerChoice === 2 ){
+        $("#text").html("Paper");
+        computerChoiceName="paper"
+}
+   if (userChoice === "rock" && computerChoiceName ==="rock")  {
+       $("#result").html ("Its A Tie")
+       (userChoice === "rock" && computerChoiceName === "paper") 
+            $("#result").html ("Paper Wins. As You Should") 
+     (userChoice === "rock" && computerChoiceName === "scissors") 
+            $("#result").html ("Rock Wins KAPOWWW") 
+       }
+       
+     if (userChoice === "paper" && computerChoiceName === "paper") {
+            $("#result").html ("Its A Tie.")
+            (userChoice === "paper" && computerChoiceName === "rock")
+            $("#result").html ("Paper Wins. As You Should") 
+            (userChoice === "paper" && computerChoiceName === "scissors")
+            $("#result").html ("Scissors WINNN OKURRRR.") 
+     }
+     
+     if (userChoice === "scissors" && computerChoiceName === "scissors") {
+          $("#result").html ("Its A Tie.")
+          (userChoice === "scissors" && computerChoiceName === "paper") 
+           $("#result").html ("Scissors WINNN OKURRR") 
+       (userChoice === "scissors" && computerChoiceName === "rock")
+            $("#result").html ("Rock Wins. KAPOWWW") 
+     }
+});
